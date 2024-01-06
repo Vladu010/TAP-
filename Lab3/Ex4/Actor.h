96a8a3771928dfa962d843ef423cec192290f253
Vladu010 <110931080+Vladu010@users.noreply.github.com>
2024-01-07 00:10:36 +0200

    Add files via upload

---
+++ Lab3/Ex4/Actor.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class Actor
+{
+private:
+	string nume;
+	string prenume;
+	int varsta;
+
+	static int nrInstanteActor;
+
+public:
+	Actor(string nume = "numeActor", string prenume = "prenumeActor", int varsta = 0);
+	Actor(const Actor& a);
+	~Actor();
+	Actor& operator=(const Actor& a);
+
+	inline void setNume(string n) { nume = n; }
+	inline string getNume() { return nume; }
+	inline int static getNrInstanteActor() { return nrInstanteActor; }
+};
+
---
+++ Lab3/Ex4/Film.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <iostream>
+#include "Actor.h"
+
+using namespace std;
+
+class Film
+{
+private:
+	string nume;
+	int nrActori;
+	double buget;
+	Actor** actori;
+	int contor_actori;
+public:
+	Film(string nume = "numeFilm", int nrActori = 0, double buget = 0.0);
+	Film(const Film& f);
+	~Film();
+	Film& operator=(const Film& f);
+
+	string getNume();
+	void setNume(string s);
+	int getNumarActori();
+	int getBuget();
+
+	void adaugaActor(Actor* a);
+	void afisareActori();
+
+	friend void actoriComuni(const Film& f1, const Film& f2);
+
+	friend class Actor;
+};
